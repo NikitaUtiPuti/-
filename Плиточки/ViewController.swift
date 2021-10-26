5b9bb244e72ef3d571b13bca73b9a475466e30a1
Nikita <ymnooi1998@yandex.ru>
2021-10-26 23:25:41 +0300

    Initial Commit

---
+++ Плиточки/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Плиточки
+//
+//  Created by Никита Зюзин on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
